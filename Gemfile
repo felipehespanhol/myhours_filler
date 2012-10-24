491310b3d2a216188b0455b498959a5a22aaa65d
Felipe Hespanhol <felipeghespanhol@gmail.com>
2012-10-24 16:41:27 -0200

    Gemfile added.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem "capybara", "~> 1.1.2"
+gem "selenium", "~> 0.2.5"
--- myhours_filler.rb
+++ myhours_filler.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 require 'rubygems'
+require 'bundler/setup'
+
 require 'capybara'
 require 'capybara/dsl'
 require 'yaml'
@@ -54,11 +56,11 @@ module MyhoursFillerModule
 				puts "Achou o dia #{ day.to_s } :)"
 				# Compare if its the first td in the table, or, in other words, check if its sunday
 				day_tr = find('a', :text => " #{day.to_s} ").find(:xpath, './/..').find(:xpath, './/..')
-				day_tr_first_child_text =  day_tr.find('td:first-child').find('a').text
+				sunday =  day_tr.find('td:first-child').find('a').text
 				# Compare if its the last td in the table, or, in other words, check if its saturday
-				day_tr_last_child_text =  day_tr.find('td:last-child').find('a').text
-				if day_tr_first_child_text != " #{day.to_s} " && day_tr_last_child_text != " #{day.to_s} "
-					puts "#{day.to_s} não é fim de semana"
+				saturday =  day_tr.find('td:last-child').find('a').text
+				if (sunday != " #{day.to_s} ") and (saturday != " #{day.to_s} ")
+					day_filling if page.has_no_link?('Edit') and page.has_no_link?('Delete')
 				else
 					puts "#{day.to_s} é fim de semana"
 				end
